575cd8a5aae77c5d877bb0d0e33589d3a23af380
putri srimulyani16 <putrisrimulyani16@gmail.com>
2021-02-07 20:56:42 +0700

    merubah php, membuat laravel

---
+++ resources/views/about-me.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr> 
+			<th> Nama </th>
+			<td>:</td>
+			<td>Putri Sri Mulyani</td>
+		</tr>
+
+		<tr> 
+			<th> Kelas </th>
+			<td>:</td>
+			<td>XI</td>
+		</tr>
+
+		<tr> 
+			<th> Jurusan </th>
+			<td>:</td>
+			<td>RPL</td>
+		</tr>
+		<tr> 
+			<th> Alamat </th>
+			<td>:</td>
+			<td>Kampung Baru</td>
+		</tr>
+			
+			
+</table>
+</body>
+</html>
